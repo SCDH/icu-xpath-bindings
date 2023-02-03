9f11145063c4e146632937eb370868cb80a5d4da
Christian Lück <christian.lueck@uni-muenster.de>
2023-02-03 21:08:18 +0100

    unit test with icu:transliterator-ids() in XSLT

--- src/test/java/de/wwu/scdh/xpath/icu/text/TransliteratorIdsTest.java
+++ src/test/java/de/wwu/scdh/xpath/icu/text/TransliteratorIdsTest.java
@@ -1,5 +1,8 @@
 package de.wwu.scdh.xpath.icu.text;
 
+import java.nio.file.Paths;
+import java.io.ByteArrayOutputStream;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,8 +14,16 @@ import net.sf.saxon.lib.ExtensionFunctionCall;
 import net.sf.saxon.om.Sequence;
 import net.sf.saxon.trans.XPathException;
 import net.sf.saxon.om.SequenceIterator;
+import net.sf.saxon.s9api.*;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+import de.wwu.scdh.xpath.icu.IcuXPathFunctionRegistry;
 
 public class TransliteratorIdsTest {
+
+    private static final String XSLT = Paths.get("xsl", "transliterator-ids.html").toFile().toString();
 
     protected ExtensionFunctionCall function;
 
@@ -29,6 +40,26 @@ public class TransliteratorIdsTest {
 	SequenceIterator iter = output.iterate();
 	// assert that we have at least one ID
 	assertNotNull(iter.next());;
+    }
+
+    @Test
+    public void testInXslt() throws SaxonApiException {
+	// set up processor and register extension functions
+	Processor processor = new Processor(false);
+	IcuXPathFunctionRegistry.register(processor);
+
+	Source stylesheet = new StreamSource(XSLT);
+	XsltCompiler compiler = processor.newXsltCompiler();
+	XsltExecutable executable = compiler.compile(stylesheet);
+	Xslt30Transformer transformer = executable.load30();
+
+	ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	// we are using the stylesheet as xml source, too
+	transformer.transform(stylesheet, processor.newSerializer(outputStream));
+
+	// we just assert that the output is not empty
+	assertFalse(outputStream.toString().isEmpty());
+	//assertEquals("", outputStream.toString());
     }
 
 }
