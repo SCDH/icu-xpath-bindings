970a2ac22728b71ca430c921f94850f5dfb0239b
Christian Lück <christian.lueck@uni-muenster.de>
2023-02-03 18:01:40 +0100

    added utility class for common code

---
+++ src/main/java/de/wwu/scdh/xpath/icu/XPathUtils.java
@@ -0,0 +1,22 @@
+package de.wwu.scdh.xpath.icu;
+
+import net.sf.saxon.om.Sequence;
+import net.sf.saxon.value.StringValue;
+import net.sf.saxon.value.AtomicValue;
+
+/**
+ * Utilities used throughout the binding library.
+ */
+public class XPathUtils {
+
+    /**
+     * Cast a argument of type <code>xs:string</code> to a {@link String}.
+     *
+     * @param arg  the argument as {@link Sequence}
+     * @return a {@link String}
+     */
+    public static String getStringArgument(Sequence arg) {
+	StringValue inputValue = (StringValue) arg;
+	return ((AtomicValue) inputValue).toString();
+    }
+}
