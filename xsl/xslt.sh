78ce09a35992b793f6682366a9f92f40b3bdcc37
Christian Lück <christian.lueck@uni-muenster.de>
2023-02-03 20:21:27 +0100

    set debug level

--- xsl/xslt.sh
+++ xsl/xslt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Ddebug="true" -cp target/lib/Saxon-HE-${saxon.version}.jar:target/lib/xmlresolver-${xmlresolver.version}.jar:target/lib/slf4j-api-${slf4j.version}.jar:target/lib/slf4j-simple-${slf4j.version}.jar:target/lib/icu4j-${icu4j.version}.jar:target/lib/icu4j-charset-${icu4j.version}.jar:target/lib/icu4j-localespi-${icu4j.version}.jar:target/${project.artifactId}-${project.version}.jar net.sf.saxon.Transform $@
+java -Ddebug="true" -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -cp target/lib/Saxon-HE-${saxon.version}.jar:target/lib/xmlresolver-${xmlresolver.version}.jar:target/lib/slf4j-api-${slf4j.version}.jar:target/lib/slf4j-simple-${slf4j.version}.jar:target/lib/icu4j-${icu4j.version}.jar:target/lib/icu4j-charset-${icu4j.version}.jar:target/lib/icu4j-localespi-${icu4j.version}.jar:target/${project.artifactId}-${project.version}.jar net.sf.saxon.Transform $@
